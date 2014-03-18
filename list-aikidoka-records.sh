a40831e43c75b5cdddea56e839fb12049d01ff68
Sanshinkai Aikido Utrecht <utrecht@sanshinkai.eu>
2014-03-18 20:17:02 +0100

    Added filtering on listing dojo and aikidoka

--- list-aikidoka-records.sh
+++ list-aikidoka-records.sh
@@ -1,1 +1,6 @@
-mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select * from aikidoka"
+if [ $# -ne 1 ]
+then
+	mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select * from aikidoka"
+else
+	mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select * from aikidoka where dojo_id = "$1
+fi
--- list-dojo-records.sh
+++ list-dojo-records.sh
@@ -1,1 +1,6 @@
-mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select * from dojo"
+if [ $# -ne 1 ]
+then
+	mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select * from dojo"
+else
+	mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select * from dojo where group_id = "$1
+fi
