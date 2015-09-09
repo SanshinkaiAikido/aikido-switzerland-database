4bf221c7561f919ade4fe65afd34028ef224dcf9
Sanshinkai Aikido Utrecht <utrecht@sanshinkai.eu>
2015-09-09 17:26:34 +0200

    Write results to file

--- histogram-aikidoka-active.sh
+++ histogram-aikidoka-active.sh
@@ -1,4 +1,4 @@
-echo -e -n "active\t"
-mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select count(id) from aikidoka where active = 1"|tail -n +5
-echo -e -n "inactive\t"
-mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select count(id) from aikidoka where active = 0"|tail -n +5
+echo -e -n "active\t" >histogram-aikidoka-active.tsv
+mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select count(id) from aikidoka where active = 1"|tail -n +5 >>histogram-aikidoka-active.tsv
+echo -e -n "inactive\t" >>histogram-aikidoka-active.tsv
+mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select count(id) from aikidoka where active = 0"|tail -n +5 >>histogram-aikidoka-active.tsv
--- histogram-aikidoka-sex.sh
+++ histogram-aikidoka-sex.sh
@@ -1,4 +1,4 @@
-echo -e -n "male\t"
-mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select count(id) from aikidoka where active = 1 and female = 0"|tail -n +5
-echo -e -n "female\t"
-mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select count(id) from aikidoka where active = 1 and female = 1"|tail -n +5
+echo -e -n "male\t" >histogram-aikidoka-sex.tsv
+mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select count(id) from aikidoka where active = 1 and female = 0"|tail -n +5 >>histogram-aikidoka-sex.tsv
+echo -e -n "female\t" >>histogram-aikidoka-sex.tsv
+mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select count(id) from aikidoka where active = 1 and female = 1"|tail -n +5 >>histogram-aikidoka-sex.tsv
