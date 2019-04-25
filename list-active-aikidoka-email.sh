b78ffde6c69e2e8d454fdbb6c0c55ca7adca046d
SanshinkaiAikido <utrecht@sanshinkai.eu>
2019-04-25 17:28:47 +0200

    improved email scripts

--- list-active-aikidoka-email.sh
+++ list-active-aikidoka-email.sh
@@ -1,6 +1,6 @@
 if [ $# -ne 1 ]
 then
-	mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select email from aikidoka where active = 1 and email != ''"
+	mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select distinct email from aikidoka where active = 1 and email != ''"
 else
-	mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select email from aikidoka where active = 1 and email != '' and dojo_id = "$1
+	mysql --silent -h `cat connection-hostname` -u `cat connection-username` -p`cat connection-password` -D `cat connection-database` -v -e "select distinct email from aikidoka where active = 1 and email != '' and dojo_id = "$1
 fi
